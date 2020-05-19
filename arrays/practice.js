a612835b893a21af4ee3671816aa0238c72cd3b5
Nahum Ducasse <losond8@gmail.com>
2020-05-18 21:09:09 -0700

    Completed
    Review Questions 5 - 7 [functions stored within an object]
    
    // Functions that return objects are called object factories
    // They're a blueprint for creating a lot of objects of one type
    // A function contained in an object is called a method
    // You can add functions to an object just like any other property
    
    Examples in the readMe file

--- arrays/practice.js
+++ arrays/practice.js
@@ -14,7 +14,11 @@
   Then alert your name using dot notation.
 */
 
-//Code here
+var me = {};
+  me.name = "Persuasions of Comfort";
+  me.age = 29;
+
+  //alert(me.name);
 
 ////////// PROBLEM 2 //////////
 
@@ -23,21 +27,31 @@
   Have the values to those keys be strings that are equal to your favorite thing in that category.
 */
 
-//Code here
+var favoriteThings = {
+    band: "JustRuss Beats",
+    food: "Puerto Rico",
+    person: "Family",
+    book: "Jeff Abbott Series",
+    movie: "ConAir",
+    holiday: "MWD or Labor Day"
+};
+
 
 /*
   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
   and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
+favoriteThings.car = "Lexus ES 2020";
+favoriteThings['brand'] = "Lexus";
 
 /*
   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
   and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
+favoriteThings.food = "Chicken Nuggets";
+favoriteThings['book'] = "Harry Potter"; 
 
 ////////// PROBLEM 3 //////////
 
@@ -49,13 +63,16 @@
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack.
 */
 
-//Code here
+var backPack = {};
+var item = "firstPocket";
+
+backPack[item] = "chapstick";
 
 /*
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
+//alert(backPack);
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -63,7 +80,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console.
 */
 
-//Code here
+console.log(backPack);
 
 ////////// PROBLEM 4 //////////
 
@@ -84,7 +101,8 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
+user2.name = 'Bryan G. Smith';
+user2.email = 'bryan.smith@devmounta.in';
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
@@ -95,20 +113,31 @@ var user2 = {
   Create an empty object called methodCollection.
 */
 
-//Code Here
+var methodCollection = {};
 
 /*
   Now add two methods (functions that are properties on objects) to your methodCollection object.
   One called 'alertHello' which alerts 'hello' and another method called 'logHello' which logs 'hello' to the console.
 */
+var methodCollection = {
 
-//Code Here
+  alertHello() {
+    return alert("hello")
+  },
+
+  logHello() {
+    console.log('hello')
+  }
+
+};
+
 
 /*
   Now call your alertHello and logHello methods.
 */
 
-//Code Here
+console.log("alertHello");
+console.log("logHello");
 
 ////////// PROBLEM 6 //////////
 
@@ -117,7 +146,13 @@ var user2 = {
   Return a new object with all of the information that you passed in.
 */
 
-//Code Here
+function makePerson (name, birthday, ssn){
+return {
+  name:name,
+  birthday: birthday,
+  ssn: ssn
+}
+}
 
 ////////// PROBLEM 7 //////////
 
@@ -126,4 +161,10 @@ var user2 = {
   Return that object so that whenever you invoke makeCard, you get a brand new credit card.
 */
 
-//Code Here
+var makeCard = (cardNumber, expirationDate, securityCode) =>{
+  return {
+    cardNumber:cardNumber,
+    expirationDate: expirationDate,
+    securityCode: securityCode
+  }
+}
